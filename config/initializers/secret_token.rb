3039bb1dac3e1de244f45857ff4cc343b214a976
tayoadeyefa <tayoadeyefa@gmail.com>
2014-03-22 07:09:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tayos1stapp::Application.config.secret_key_base = '58ceed40da8c9e55334ef92c470cc8a1445e843eb4a154401ceb42c7c54979e332998ae79f695f05ba32139b34f520206a4fa7a7ddcbb11eb9204d743dac76ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tayos1stapp::Application.config.session_store :cookie_store, key: '_tayos1stapp_session'
